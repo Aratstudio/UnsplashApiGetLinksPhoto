b0c933e0316eac7dba2d5f097183555a7df180d2
Arat Mizhit-Dorzhu <mizhitd@gmail.com>
2019-05-21 02:28:55 +0700

    Optimization and add features

--- unspl.php
+++ unspl.php
@@ -1,8 +1,18 @@
 <?php
 
+//unsplash-------------------------
+
 $APIKey ='11f2ff5a50fcce4df43aa4c897d132d3f5ad4a84ed0aec7be67718deb5120192';
 
-$getApi=file_get_contents('https://api.unsplash.com/search/photos?query=chicago&per_page=50&client_id=11f2ff5a50fcce4df43aa4c897d132d3f5ad4a84ed0aec7be67718deb5120192');
+$query = array('apple','wild','mountain');
+
+// $query2 = array('' => , );
+
+
+//https://api.unsplash.com/search/photos?query=chicago&per_page=50&client_id=11f2ff5a50fcce4df43aa4c897d132d3f5ad4a84ed0aec7be67718deb5120192
+
+$getApi=file_get_contents('https://api.unsplash.com/search/photos?query='.$query[2].'&per_page=5&client_id='.$APIKey);
+echo($getApi);
 $data = json_decode($getApi);
 foreach ($data as $key => $value) {
 
@@ -15,6 +25,9 @@ foreach ($data as $key => $value) {
 
 
     $unspPhoto0=$value[0]->urls->full;
+    $unspPhotoTags0=$value[0]->tags[0]->title;
+    $unspPhotoTags1=$value[0]->tags[1]->title;
+    $unspPhotoTags2=$value[0]->tags[2]->title;
     $unspPhoto1=$value[1]->urls->full;
     $unspPhoto2=$value[2]->urls->full;
     $unspPhoto3=$value[3]->urls->full;
@@ -28,18 +41,17 @@ foreach ($data as $key => $value) {
 }
 
 echo($unspPhoto0),'<br>';
+ echo($unspPhotoTags0),'<br>';
+ echo($unspPhotoTags1),'<br>';
+ echo($unspPhotoTags2),'<br>';
+
 echo($unspPhoto1),'<br>';
 echo($unspPhoto2),'<br>';
 echo($unspPhoto3),'<br>';
 echo($unspPhoto4),'<br>';
 
 
-
-
-
-
-
-
+//конец unsplash-------------------------
 
 
 ?>
