de8c0c0889f60c535893f53b6dc9e49d2c66b42b
Arat Mizhit-Dorzhu <mizhitd@gmail.com>
2019-05-21 06:12:28 +0700

    work) i am stupid)) ahah

--- mb_edit.php
+++ mb_edit.php
@@ -36,9 +36,13 @@ $query = array('apple','wild','mountain','blue','cloud','car','bird','yellow','m
 //https://api.unsplash.com/search/photos?query=chicago&per_page=50&client_id=11f2ff5a50fcce4df43aa4c897d132d3f5ad4a84ed0aec7be67718deb5120192
 
 $getApi=file_get_contents('https://api.unsplash.com/search/photos?query='.$query[array_rand($query)].'&per_page=5&client_id='.$APIKey);
-echo($getApi);
+//echo($getApi);
 $data = json_decode($getApi);
 foreach ($data as $key => $value) {
+
+    
+//обязательный, так как дает ссылку для скачивания
+$unspPhoto0=$value[0]->urls->full;
 
 //работает, тут просто вывод ссылок картинок
     // echo( $value[0]->urls->full), '<br>';
@@ -48,17 +52,20 @@ foreach ($data as $key => $value) {
     // echo( $value[4]->urls->full), '<br>';
 
 
-    $unspPhoto0=$value[0]->urls->full;
-    $unspPhotoTags0=$value[0]->tags[0]->title;
-    $unspPhotoTags0=$value[0]->tags[1]->title;
-    $unspPhotoTags0=$value[0]->tags[0]->title;
+    $unspPhotoFullDecr=$value[0]->description;
+    $unspPhotoDecr=$value[0]->alt_description;
+
+    $concatenunspPhotoDecr=$unspPhotoFullDecr. '.'.$unspPhotoDecr;
+   
+    // $unspPhotoTags0=$value[0]->tags[1]->title;
+    // $unspPhotoTags0=$value[0]->tags[0]->title;
     
-    $unspPhotoTags1=$value[0]->tags[1]->title;
-    $unspPhotoTags2=$value[0]->tags[2]->title;
-    $unspPhoto1=$value[1]->urls->full;
-    $unspPhoto2=$value[2]->urls->full;
-    $unspPhoto3=$value[3]->urls->full;
-    $unspPhoto4=$value[4]->urls->full;
+    // $unspPhotoTags1=$value[0]->tags[1]->title;
+    // $unspPhotoTags2=$value[0]->tags[2]->title;
+    // $unspPhoto1=$value[1]->urls->full;
+    // $unspPhoto2=$value[2]->urls->full;
+    // $unspPhoto3=$value[3]->urls->full;
+    // $unspPhoto4=$value[4]->urls->full;
 
 //проверка
     // print_r( $value[1]->urls->full);
@@ -67,15 +74,15 @@ foreach ($data as $key => $value) {
 
 }
 
-echo($unspPhoto0),'<br>';
- echo($unspPhotoTags0),'<br>';
- echo($unspPhotoTags1),'<br>';
- echo($unspPhotoTags2),'<br>';
+// echo($unspPhoto0),'<br>';
+//  echo($unspPhotoTags0),'<br>';
+//  echo($unspPhotoTags1),'<br>';
+//  echo($unspPhotoTags2),'<br>';
 
-echo($unspPhoto1),'<br>';
-echo($unspPhoto2),'<br>';
-echo($unspPhoto3),'<br>';
-echo($unspPhoto4),'<br>';
+// echo($unspPhoto1),'<br>';
+// echo($unspPhoto2),'<br>';
+// echo($unspPhoto3),'<br>';
+// echo($unspPhoto4),'<br>';
 
 
 //конец unsplash-------------------------
@@ -122,7 +129,7 @@ $attachments = sprintf('photo%s_%s', $save[0]['owner_id'], $save[0]['id']);
 $post = $vk->wallPost([
     'owner_id' => "-$group_id",
     'from_group' => 1,
-    'message' => $unspPhotoTags0,
+    'message' => $concatenunspPhotoDecr,
     'attachments' => $attachments
    
 
